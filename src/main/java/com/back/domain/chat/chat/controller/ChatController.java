794473d60ed729fc5fb137dd196f6c4964e9d372
luckhee <dcs1206@naver.com>
2025-07-24 22:11:16 +0900

    [feat] ChatService 구현

--- src/main/java/com/back/domain/chat/chat/controller/ChatController.java
+++ src/main/java/com/back/domain/chat/chat/controller/ChatController.java
@@ -1,17 +1,22 @@
 package com.back.domain.chat.chat.controller;
 
 import com.back.domain.chat.chat.dto.MessageDto;
+import com.back.domain.chat.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor
 public class ChatController {
+
+    private final ChatService chatService;
+
 
     @MessageMapping("/sendMessage")
     @SendTo("/topic/messages")
     public MessageDto sendMessage(MessageDto chatMessage) {
-
         return chatMessage;
     }
 }
---
+++ src/main/java/com/back/domain/chat/chat/service/ChatService.java
@@ -0,0 +1,7 @@
+package com.back.domain.chat.chat.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatService {
+}
