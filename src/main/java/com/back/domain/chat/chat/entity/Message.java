828a6e94c596d91d5240e730e62caf5fe2a0c89d
luckhee <dcs1206@naver.com>
2025-07-24 10:27:22 +0900

    [refactor] chat관련 entity 수정

--- src/main/java/com/back/domain/chat/chat/entity/Message.java
+++ src/main/java/com/back/domain/chat/chat/entity/Message.java
@@ -1,18 +1,24 @@
 package com.back.domain.chat.chat.entity;
 
+import com.back.domain.member.entity.Member;
 import com.back.global.jpa.entity.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
+@Getter
+@Setter
 public class Message extends BaseEntity {
     //member, chatroom 관계 설정
     @ManyToOne
     private ChatRoom chatRoom;
 
-    private String senderName;
+    @ManyToOne
+    private Member Senter;
 
     private String content;
 
