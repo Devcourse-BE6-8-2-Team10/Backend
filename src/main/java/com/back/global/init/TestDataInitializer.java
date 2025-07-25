0339281c3362aab4e74acdbad4fec4b52391453c
ahdeka <ahdeka01@gmail.com>
2025-07-25 14:45:17 +0900

    [refactor] Dev/Test용 DataInitializer 리팩토링

--- src/main/java/com/back/global/init/TestDataInitializer.java
+++ src/main/java/com/back/global/init/TestDataInitializer.java
@@ -5,55 +5,35 @@ import com.back.domain.member.entity.Role;
 import com.back.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+@Profile("test")
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class DataInitializer {
+public class TestDataInitializer implements ApplicationRunner {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
-    private final ApplicationContext context;
 
-
-    @Bean
-    public ApplicationRunner baseInitDataApplicationRunner() {
-        return args -> {
-            log.info("=====데이터 초기화 시작=====");
-            DataInitializer self = context.getBean(DataInitializer.class);
-            self.initAdmin();
-            self.initUser();
-            log.info("=====데이터 초기화 완료=====");
-        };
-    }
-
-
-    // 관리자 계정 생성
+    @Override
     @Transactional
-    public void initAdmin() {
-        if (!memberRepository.existsByEmail("admin@admin.com")) {
-            Member admin = Member.builder()
-                    .email("admin@admin.com")
-                    .password(passwordEncoder.encode("admin1234!"))
-                    .name("관리자")
-                    .role(Role.ADMIN) // enum 또는 String 값 사용
-                    .build();
-            memberRepository.save(admin);
-            log.info("관리자 계정이 생성되었습니다.");
-        }
+    public void run(ApplicationArguments args) {
+        log.info("===== TEST 데이터 초기화 시작=====");
+        initUser();
+        log.info("===== TEST 데이터 초기화 완료=====");
     }
 
     // 일반 사용자 계정 생성
     @Transactional
     public void initUser() {
-        createUserIfNotExists("user1@user.com", "사용자1");
-        createUserIfNotExists("user2@user.com", "사용자2");
+        createUserIfNotExists("testuser1@user.com", "사용자1");
+        createUserIfNotExists("testuser2@user.com", "사용자2");
     }
 
     private void createUserIfNotExists(String email, String name) {
