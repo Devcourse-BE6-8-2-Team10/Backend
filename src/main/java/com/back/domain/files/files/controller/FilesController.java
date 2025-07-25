aa254c897cf53e37459262ca6c63239e87645e88
NewplayerKOR <149351553+NewplayerKOR@users.noreply.github.com>
2025-07-25 15:05:45 +0900

    [feat#67] : Swagger 파일 첨부 UI

--- src/main/java/com/back/domain/files/files/controller/FilesController.java
+++ src/main/java/com/back/domain/files/files/controller/FilesController.java
@@ -4,8 +4,10 @@ import com.back.domain.files.files.dto.FileUploadResponseDto;
 import com.back.domain.files.files.service.FilesService;
 import com.back.domain.member.service.MemberService;
 import com.back.global.rsData.RsData;
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,9 +22,10 @@ public class FilesController {
     private final MemberService memberService;
 
     // 파일 업로드 (게시글 저장 -> postId 받음, 이미지 저장)
-    @PostMapping("/{postId}/files")
+    @PostMapping(value = "/{postId}/files", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public RsData<List<FileUploadResponseDto>> uploadFiles(
             @PathVariable @Positive long postId,
+            @Parameter (description = "업로드할 파일들", required = false)
             @RequestPart(value = "files", required = false) MultipartFile[] files
     ) {
         return filesService.uploadFiles(postId, files);
