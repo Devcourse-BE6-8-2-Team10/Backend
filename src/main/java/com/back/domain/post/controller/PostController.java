35fe2467abac62c5c1ec6e70e71f20cc85979d66
Code <yna4371@gmail.com>
2025-07-30 16:05:45 +0900

    [feat] : 게시글 상세 목록 조회

--- src/main/java/com/back/domain/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/controller/PostController.java
@@ -1,10 +1,15 @@
 package com.back.domain.post.controller;
 
+import com.back.domain.member.entity.Member;
 import com.back.domain.post.dto.PostDetailDTO;
 import com.back.domain.post.dto.PostListDTO;
 import com.back.domain.post.dto.PostRequestDTO;
 import com.back.domain.post.service.PostService;
+import com.back.global.rq.Rq;
+import com.back.global.rsData.RsData;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
+import com.back.global.exception.ServiceException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,16 +23,39 @@ import java.util.List;
 public class PostController {
 
     private final PostService postService;
+    private final Rq rq;
 
     //게시글 등록
+    @Operation(summary = "게시글 등록")
     @PostMapping
     public ResponseEntity<PostDetailDTO> createPost(@Valid @RequestBody PostRequestDTO dto) {
         PostDetailDTO result = postService.createPost(dto);
+
         URI location = URI.create("/api/posts/" + result.id());
         return ResponseEntity.created(location).body(result);
         }
 
-    //인기 게시글 top 10 보여줌
+    //게시글 목록 조회
+    @Operation(summary = "게시글 목록 조회")
+    @GetMapping
+    public ResponseEntity<List<PostListDTO>> getPostList() {
+        List<PostListDTO> result = postService.getPostList();
+        return ResponseEntity.ok(result);
+    }
+
+    //게시글 상세 조회
+    @GetMapping("/{postId}")
+    @Operation(summary = "게시글 상세 조회")
+    public RsData<PostDetailDTO> getPostDetail(@PathVariable Long postId) {
+        Member member = rq.getMember();
+        if (member == null) {
+            throw new ServiceException("UNAUTHORIZED", "로그인이 필요합니다.");
+        }
+        return postService.getPostDetail(postId, member);
+    }
+
+    //인기 게시글 보여줌
+    @Operation(summary = "인기 게시글 조회")
     @GetMapping("/popular")
     public ResponseEntity<List<PostListDTO>> getTop10PopularPosts() {
         List<PostListDTO> result = postService.getTop10PopularPosts();
