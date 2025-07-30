46fc6f2f35264a8e50d65f3407de13ccc1e47d3f
luckhee <dcs1206@naver.com>
2025-07-30 12:38:35 +0900

    [fix] redis에서 메시지 수신하고 한번더 직렬화(이중 직렬화)해결

--- src/main/java/com/back/domain/chat/chat/dto/MessageDto.java
+++ src/main/java/com/back/domain/chat/chat/dto/MessageDto.java
@@ -1,17 +1,16 @@
 package com.back.domain.chat.chat.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
+import lombok.Setter;
 
 import java.util.Objects;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class MessageDto {
     private Long senderId;
     private Long chatRoomId;
@@ -19,10 +18,26 @@ public class MessageDto {
     private String senderName;
     private String senderEmail;
     private String content;
-    
+
     // Jackson JSON 역직렬화를 위한 sender 필드 (senderName과 동일)
     @JsonProperty("sender")
     private String sender;
+
+    @JsonCreator
+    public MessageDto(
+            @JsonProperty("senderId") Long senderId,
+            @JsonProperty("chatRoomId") Long chatRoomId,
+            @JsonProperty("senderName") String senderName,
+            @JsonProperty("senderEmail") String senderEmail,
+            @JsonProperty("content") String content,
+            @JsonProperty("sender") String sender) {
+        this.senderId = senderId;
+        this.chatRoomId = chatRoomId;
+        this.senderName = senderName;
+        this.senderEmail = senderEmail;
+        this.content = content;
+        this.sender = sender != null ? sender : senderName; // sender가 null이면 senderName 사용
+    }
 
     public MessageDto(String senderName, String content, Long senderId, Long chatRoomId) {
         this.senderName = senderName;
@@ -37,7 +52,7 @@ public class MessageDto {
         this.senderName = senderName;
         this.sender = senderName;
     }
-    
+
     public void setSender(String sender) {
         this.sender = sender;
         this.senderName = sender;
