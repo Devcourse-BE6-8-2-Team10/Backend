d46131063f0b1325c8511b4c94383ad49e37997d
user.email <younsoo6103@naver.com>
2025-08-04 09:58:42 +0900

    [refactor] Trade 테스트 코드 수정

--- src/test/java/com/back/domain/trade/controller/AdmTradeControllerTest.java
+++ src/test/java/com/back/domain/trade/controller/AdmTradeControllerTest.java
@@ -13,13 +13,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
@@ -50,7 +50,7 @@ class AdmTradeControllerTest {
     private TradeRepository tradeRepository;
     @Autowired
     private Rq rq;
-    @MockitoBean
+    @MockBean
     private FileStorageService fileStorageService;
 
     @Test
